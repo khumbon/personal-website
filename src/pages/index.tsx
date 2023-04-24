b7ef720ff5b2826ba13cc84671fdcdbc08f554f3
Khumbo Nyirenda <khumbon@gmail.com>
2023-04-25 00:03:42 +0100

    Removed portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
-import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
@@ -21,7 +20,6 @@ const Home = (): JSX.Element => {
       <Hero />
       <About />
       <Resume />
-      <Portfolio />
       <Testimonials />
       <Contact />
       <Footer />
